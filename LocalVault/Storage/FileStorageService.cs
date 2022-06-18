74007130e0e55ba3e9c978b65dd831cab5326d0a
Patryk Łazowski <jakosziewski@gmail.com>
2022-06-18 12:13:24 +0200

    Added Load and Save method for interface

--- LocalVault/Storage/FileStorageService.cs
+++ LocalVault/Storage/FileStorageService.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace LocalVault.Storage
 {
-    public class FileStorageService : IStorageService
+    public class FileStorageService : IStorageService<PasswordEntry>
     {
+	    private readonly string filename;
+	    public FileStorageService(string filename)
+	    {
+		    this.filename = filename;
+	    }
+
+	    public List<PasswordEntry> LoadAll()
+	    {
+		    using StreamReader reader = File.OpenText(filename);
+		    string fileContent = reader.ReadToEnd();
+
+	    }
+
+	    public void SaveAll(List<PasswordEntry> entries)
+	    {
+		    throw new NotImplementedException();
+	    }
     }
 }
--- LocalVault/Storage/IStorageService.cs
+++ LocalVault/Storage/IStorageService.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace LocalVault.Storage
 {
-    public interface IStorageService
+    public interface IStorageService<T>
     {
+	    List<T> LoadAll();
+	    void SaveAll(List<T> entries);
     }
 }
