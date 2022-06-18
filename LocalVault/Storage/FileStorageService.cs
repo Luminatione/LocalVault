db4451515e50346a65de2f19fc86ede46b877108
Patryk Łazowski <jakosziewski@gmail.com>
2022-06-18 20:31:39 +0200

    Implemented PasswordEntry read and load from file

--- LocalVault/Storage/FileStorageService.cs
+++ LocalVault/Storage/FileStorageService.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace LocalVault.Storage
 {
-    public class FileStorageService : IStorageService<PasswordEntry>
+    public class FileStorageService : IStorageService
     {
 	    private readonly string filename;
 	    public FileStorageService(string filename)
@@ -17,12 +18,13 @@ namespace LocalVault.Storage
 	    {
 		    using StreamReader reader = File.OpenText(filename);
 		    string fileContent = reader.ReadToEnd();
-
+			return JsonConvert.DeserializeObject<List<PasswordEntry>>(fileContent);
 	    }
 
 	    public void SaveAll(List<PasswordEntry> entries)
 	    {
-		    throw new NotImplementedException();
+		    using StreamWriter reader = new StreamWriter(File.OpenWrite(filename));
+		    reader.Write(JsonConvert.SerializeObject(entries));
 	    }
     }
 }
