77949f724e6b3356b8fcc89f1355228175271474
Patryk Łazowski <jakosziewski@gmail.com>
2022-06-18 12:13:58 +0200

    Object storing user data

---
+++ LocalVault/PasswordEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+
+namespace LocalVault
+{
+	public class PasswordEntry
+	{
+		public string Password { get; private set; }
+		public string Name { get; private set; }
+		public string Description { get; private set; }
+
+		public PasswordEntry(string password, string name, string description)
+		{
+			Password = password;
+			Name = name;
+			Description = description;
+		}
+	}
+}
